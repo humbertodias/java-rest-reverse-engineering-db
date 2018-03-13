d638b2979ba95d769b6ea453dfc88ab3a3aec47c
Unknown <humberto.dias@techne.com.br>
2018-03-14 00:11:37 +0100

    Initial Commit

---
+++ src/main/java/com/example/demohateoas/DemoHateoasApplication.java
@@ -0,0 +1,12 @@
+package com.example.demohateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHateoasApplication.class, args);
+	}
+}
